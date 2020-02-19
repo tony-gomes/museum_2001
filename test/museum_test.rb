1b63ba73b7b21e11ddb9575e099651b921a19129
Anthony Gomes <code@tonygom.es>
2020-02-19 11:01:08 -0700

    Add more museum tests

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -114,4 +114,64 @@ class MuseumTest < MiniTest::Test
 
     assert_equal expected, @dmns.patrons_by_exhibit_interest
   end
+
+  def test_it_can_get_lottery_contestants
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dmns.add_exhibit(gems_and_minerals)
+
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @dmns.add_exhibit(dead_sea_scrolls)
+
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    @dmns.add_exhibit(imax)
+
+    assert_equal [], @dmns.patrons
+
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    @dmns.admit(patron_1)
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    @dmns.admit(patron_2)
+
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(patron_3)
+
+    assert_equal [patron_1, patron_3], @dmns.ticket_lottery_contestants(dead_sea_scrolls)
+  end
+
+  def test_it_can_draw_lottery_winner
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dmns.add_exhibit(gems_and_minerals)
+
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @dmns.add_exhibit(dead_sea_scrolls)
+
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    @dmns.add_exhibit(imax)
+
+    assert_equal [], @dmns.patrons
+
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    @dmns.admit(patron_1)
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    @dmns.admit(patron_2)
+
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(patron_3)
+
+    assert_equal expected, @dmns.draw_lottery_winner(dead_sea_scrolls)
+    assert_nil @dmns.draw_lottery_winner(gems_and_minerals)
+  end
+
+  def test_it_can_announce_lottery_winner
+  end
 end
