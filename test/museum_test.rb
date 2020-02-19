1d4da66d6bac620b13d8e11bb57f9cfac287db3b
Anthony Gomes <code@tonygom.es>
2020-02-19 10:21:46 -0700

    Add more museum tests

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -51,4 +51,67 @@ class MuseumTest < MiniTest::Test
 
     assert_equal [imax], @dmns.recommend_exhibits(patron_2)
   end
+
+  def test_it_can_admit_patrons
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dmns.add_exhibit(gems_and_minerals)
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @dmns.add_exhibit(dead_sea_scrolls)
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    @dmns.add_exhibit(imax)
+
+    assert_equal [], @dmns.patrons
+
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+
+    @dmns.admit(patron_1)
+    @dmns.admit(patron_2)
+    @dmns.admit(patron_3)
+
+    expected = [patron_1, patron_2, patron_3]
+    assert_equal expected, @dmns.patrons
+  end
+
+  def test_it_can_get_patrons_by_exhibit_interests
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dmns.add_exhibit(gems_and_minerals)
+
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @dmns.add_exhibit(dead_sea_scrolls)
+
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    @dmns.add_exhibit(imax)
+
+    assert_equal [], @dmns.patrons
+
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+
+    @dmns.admit(patron_1)
+    @dmns.admit(patron_2)
+    @dmns.admit(patron_3)
+
+    expected = {
+      gems_and_minerals => [patron_1],
+      dead_sea_scrolls => [patron_1, patron_2, patron_3],
+      imax => []
+    }
+
+    assert_equal expected, @dmns.patrons_by_exhibit_interest
+  end
 end
